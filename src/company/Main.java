02edcf455c2254f5a1ce1daf04a72c5049195f06
SALEM .. <salemsnsalmutairi@gmail.com>
2024-11-05 15:00:51 +0300

    initial

--- src/company/Main.java
+++ src/company/Main.java
@@ -5,9 +5,20 @@ public class Main {
         Employee emily = new Employee("Emily", "Finance", 50000);
         Manager john = new Manager("John", "IT", 75000, 5);
         Intern dave = new Intern("Dave", "Marketing", 30000, "State University");
+        Director salem = new Director("salem","tech",999999.1, 5);
+        Engineer osama = new Engineer("Osama","cyber",500,new String[]{
+                "ROM piracy", "installing illegal software"
+        });
+        SalesPerson fajri = new SalesPerson("Fajri", "sales",2.750, 1);
+
+
 
         System.out.println(emily);
         System.out.println(john);
         System.out.println(dave);
+        System.out.println(salem);
+        System.out.println(osama);
+        System.out.println(fajri);
+
     }
 }
